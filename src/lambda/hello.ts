babcf0de18cc47342b52d4cca9daaf0835e87e7a
junki akutagawa <akutagawa.junki@ditgroup.jp>
2023-04-25 14:22:38 +0900

    add express

--- src/lambda/hello.ts
+++ src/lambda/hello.ts
@@ -1,9 +1,22 @@
+import serverlessExpress from "@vendia/serverless-express"
+import express from "express"
+import cors from "cors"
 import { message } from "../model/message";
 
-export const handler = async (event: any = {}): Promise<any> => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: message(event.path)
-  };
-}
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("hello", (event, res) => {
+  res.status(200).send({
+    message: "" + message(event.path)
+  })
+})
+
+app.get('/bye', (_, res) => {
+  res.status(200).send({
+    message: 'byebye',
+  })
+})
+
+exports.handler = serverlessExpress({ app })
